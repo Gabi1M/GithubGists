60c0977685f2f0b09fe5d4b5d22d514f36e3aff8
Gabi Macovei <ig_999@yahoo.com>
2021-07-12 13:09:33 +0300

    Added global context and API request helper

---
+++ src/tools/ApiHelper.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export const useFetch = (withAuth) => {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [url, setUrl] = useState(null);
+    useEffect(() => {
+        const fetchData = () => {
+            try {
+                if (url !== null) {
+                    if (withAuth) {
+                        axios.get(url).then((res) => {
+                            setResponse(res.data);
+                        });
+                    } else {
+                        axios.get(url).then((res) => {
+                            setResponse(res.data);
+                        });
+                    }
+                }
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [url]);
+    return { response, error, loading, setUrl };
+};
